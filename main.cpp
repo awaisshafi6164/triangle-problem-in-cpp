5f2162ae168652a3c31e14a28bf518d618071d2f
Muhammad Awais Shafi <awaisshafi6164@gmail.com>
2022-08-10 17:02:39 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 #include<time.h>
 #include<vector>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #include<graphics.h>
 using namespace std;
 
+double area(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+    return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
+}
+
 int main()
 {
-
     cout<<"\tTriangle Problem\n\n";
 
     srand(time(0));
@@ -25,28 +28,48 @@ int main()
 
     cout<<"Random Generated Values after sorting: "<<vec[0]<<", "<<vec[1]<<", "<<vec[2]<<"\n";
 
-    int no;
-    cout<<"Enter any Number Between (1-100): ";
-    cin>>no;
+    int x0 = 0;
+    cout<<"Enter any Number (x0) (0 - 100): ";
+    cin>>x0;
+    int y0 = 0;
+    cout<<"Enter any Number (y0) (0 - 100): ";
+    cin>>y0;
+
+    double A, A1, A2, A3;
+
+    A = area(vec[2], vec[2], vec[1], 0, vec[0], vec[0]);
+    A1 = area(x0, y0, vec[1], 0, vec[0], vec[0]);
+    A2 = area(vec[2], vec[2], x0, y0, vec[0], vec[0]);
+    A3 = area(vec[2], vec[2], vec[1], 0, x0, y0);
+
+    cout<<"Area Of Triangle: "<< A <<endl;
+    cout<<"A = A1 + A2 + A3 = "<< A1 + A2 + A3<<endl;
+
+    if(A == (A1 + A2 + A3)){
+        cout<<"Given point lies inside the triangle.\n\n";
+    }
+    else{
+        cout<<"Given triangle lies outside the triangle.\n\n";
+    }
 
     int gd=DETECT,gm;
     initgraph(&gd,&gm,"");
 
-    line(vec[0], 0, vec[1], vec[1]);
-    line(vec[1], vec[1], vec[2], 0);
-    line(vec[2], 0, vec[0], 0);
+    /*
+    circle(x0, y0, 2);
 
-    if(no > vec[1] && no <= vec[0]){
-            line(no, 50, no, 0);
-    }
-    else if(no > vec[2] && no <= vec[1]){
-            line(vec[1], no, 150, no);
-    }
-    else if(no <= vec[2] && no >= 1){
-            line(vec[1], no, 0, no);
-    }else{
-        cout<<"\n\nIt does not lie in triangle.\n";
-    }
+    line(vec[2], vec[2], vec[1], 0);
+    line(vec[1], 0, vec[0], vec[0]);
+    line(vec[0], vec[0], vec[2], vec[2]);
+    */
+
+    int c = 100;
+
+    circle(x0+c, y0+c, 2);
+
+    line(vec[2]+c, vec[2]+c, vec[1]+c, 0+c);
+    line(vec[1]+c, 0+c, vec[0]+c, vec[0]+c);
+    line(vec[0]+c, vec[0]+c, vec[2]+c, vec[2]+c);
 
     getch();
     return 0;
